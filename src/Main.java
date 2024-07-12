0e2474b711d0ef419770c7f46661df84f822b056
Eng. AbdulRahman <102804279+AbdulrahmanAlshammari@users.noreply.github.com>
2024-07-12 15:56:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+interface Workable {
+    void work();
+}
+
+interface Eatable {
+    void eat();
+}
+///ghggg
+class HumanWorker implements Workable {
+    public void work() {
+        System.out.println("Human working...");
+    }
+
+    public void eat() {
+        System.out.println("Human eating...");
+    }
+}
+
+class RobotWorker implements Workable {
+    public void work() {
+        System.out.println("Robot working...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        HumanWorker human = new HumanWorker();
+        RobotWorker robot = new RobotWorker();
+
+        human.work();
+        human.eat();
+
+        robot.work();
+        // robot.eat(); // This method doesn't exist for RobotWorker, as it should be.
+    }
+}
